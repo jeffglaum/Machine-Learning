9593b036e9ce0ebb498c11b1df3a0785226ffc9c
Jeff Glaum <jeffglaum@live.com>
2023-03-05 14:47:54 -0800

    Cleaned up code and added comments.

--- CNN/CIFAR10_classifier/test.py
+++ CNN/CIFAR10_classifier/test.py
@@ -10,76 +10,84 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-ssl._create_default_https_context = ssl._create_unverified_context
-
-
+# Displays CIFAR10 image.
 def imshow(img):
-    img = img / 2 + 0.5     # unnormalize
+    img = img / 2 + 0.5
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
 
+# Without this line data set downloads will encounter an SSL error.
+ssl._create_default_https_context = ssl._create_unverified_context
 
+# Find the best hardware accelerator to use (TODO: mps on the mac is slower than cpu).
+#device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Using {device} device")
+
+# Shrink the CIFAR10 images to half size to save processing time.
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-
 batch_size = 4
 
+# Find or fetch the CIFAR10 test set.
 testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
 testloader = DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=0)
 
-images, labels = next(iter(testloader))
-
+# CIFAR10 image set classes.
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-# print images
-imshow(torchvision.utils.make_grid(images))
-print('GroundTruth: ', ' '.join(f'{classes[labels[j]]:5s}' for j in range(4)))
-
+# Construct the network, recalling model parameters from storage.
 PATH = './cifar_net.pth'
-
-net = Net()
+net = Net().to(device)
 net.load_state_dict(torch.load(PATH))
 
+# Show and example batch of images and inference result.
+images, labels = next(iter(testloader))
+images, labels = images.to(device), labels.to(device)
+imshow(torchvision.utils.make_grid(images))
+print('GroundTruth: ', ' '.join(f'{classes[labels[j]]:5s}' for j in range(4)))
 outputs = net(images)
 _, predicted = torch.max(outputs, 1)
-
 print('Predicted: ', ' '.join(f'{classes[predicted[j]]:5s}'
                               for j in range(4)))
 
 correct = 0
 total = 0
 
-# since we're not training, we don't need to calculate the gradients for our outputs
+# Since we're not training, we don't need to calculate the gradients for our outputs.
 with torch.no_grad():
     for data in testloader:
         images, labels = data
-        # calculate outputs by running images through the network
+        images, labels = images.to(device), labels.to(device)
+
+        # Calculate outputs by running images through the network.
         outputs = net(images)
         # the class with the highest energy is what we choose as prediction
         _, predicted = torch.max(outputs.data, 1)
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
 
-print(f'Accuracy of the network on the 10000 test images: {100 * correct // total} %')
+print(f'Accuracy of the network on the {total} test images: {100 * correct // total} %')
 
-# prepare to count predictions for each class
+# Prepare to count predictions for each class.
 correct_pred = {classname: 0 for classname in classes}
 total_pred = {classname: 0 for classname in classes}
 
-# again no gradients needed
 with torch.no_grad():
     for data in testloader:
         images, labels = data
+        images, labels = images.to(device), labels.to(device)
+
         outputs = net(images)
         _, predictions = torch.max(outputs, 1)
-        # collect the correct predictions for each class
+
+        # Collect the correct predictions for each class.
         for label, prediction in zip(labels, predictions):
             if label == prediction:
                 correct_pred[classes[label]] += 1
             total_pred[classes[label]] += 1
 
-
-# print accuracy for each class
+# Print accuracy for each class.
 for classname, correct_count in correct_pred.items():
     accuracy = 100 * float(correct_count) / total_pred[classname]
     print(f'Accuracy for class: {classname:5s} is {accuracy:.1f} %')
--- CNN/CIFAR10_classifier/train.py
+++ CNN/CIFAR10_classifier/train.py
@@ -7,35 +7,41 @@ import torch.nn as nn
 import torch.optim as optim
 import ssl
 
-
+# Without this line data set downloads will encounter an SSL error.
 ssl._create_default_https_context = ssl._create_unverified_context
 
-transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+# Find the best hardware accelerator to use (TODO: mps on the mac is slower than cpu).
+#device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Using {device} device")
 
+# Shrink the CIFAR10 images to half size to save processing time.
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 batch_size = 4
 
+# Find or fetch the CIFAR10 training set.
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
 trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=0)
 
-testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
-testloader = DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=0)
-
+# CIFAR10 image set classes.
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-classifier_net = Net()
-
+# Construct the network and optimizer.
+classifier_net = Net().to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(classifier_net.parameters(), lr=0.001, momentum=0.9)
 
+# Train over 10 epocs.
+for epoch in range(10):
 
-for epoch in range(2):
     running_loss = 0.0
 
-    for i, data in enumerate(trainloader, 0):
-        inputs, labels = data
+    # Enumerate over the training data in batches.
+    for i, (inputs, labels) in enumerate(trainloader, 0):
+
+        inputs, labels = inputs.to(device), labels.to(device)
 
         optimizer.zero_grad()
-
         outputs = classifier_net(inputs)
         loss = criterion(outputs, labels)
         loss.backward()
@@ -43,10 +49,12 @@ for epoch in range(2):
 
         running_loss += loss.item()
         if i % 2000 == 1999:
-            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+            print(f'[epoch {epoch + 1}, iteration {i + 1:5d}] loss: {running_loss / 2000:.3f}')
             running_loss = 0.0
 
-print('Finished Training')
+print('Finished training')
 
+# Save the trained model.
 PATH = './cifar_net.pth'
 torch.save(classifier_net.state_dict(), PATH)
+print('Trained model saved')
